64504278964d7c7545bfaf4e1534c32a0d16d753
ThimoBauer <37835139+ThimoBauer@users.noreply.github.com>
2018-05-02 09:54:24 +0200

    mhm....

--- src/main/java/ueb06/Arrays.java
+++ src/main/java/ueb06/Arrays.java
@@ -5,13 +5,15 @@ class Arrays {
 	 * Gibt eine Stringrepraesentation des Arrays zurueck, z.B. [] oder [1, 2, 3]
 	 */
 	static <T> String toString(T[] array) {
-		return null;
+		if (array == null)
+			return null;
 	}
 
 	/**
 	 * Gib zurück, ob ein Objekt in dem Array enthalten ist; verwendet `equals`
 	 */
 	static <T> boolean contains(T[] array, T object) {
+
 		return false;
 	}
 }
